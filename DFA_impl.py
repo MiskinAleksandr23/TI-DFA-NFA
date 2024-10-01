1c306b56dbbcf45a93f726a950c29604b3bb2a1e
Miskin Alex <st106116@student.spbu.ru>
2024-10-01 21:29:42 +0300

    done

---
+++ DFA_impl.py
@@ -0,0 +1,30 @@
+class DFA:
+    def __init__(self, n: int, m: int):
+        self.n = n
+        self.m = m
+        self.accepted = [False for i in range(n)]
+        self.transitions = [[-1 for j in range(m)] for i in range(n)]
+        self.start = -1
+
+    def add_transition(self, _from: int, _to: int, symbol: int) -> None:
+        assert 0 <= _from < self.n and 0 <= _to < self.n and 0 <= symbol < self.m
+        self.transitions[_from][symbol] = _to
+
+    def set_accepted(self, accepted_list):
+        for vertex in accepted_list:
+            assert 0 <= vertex < self.n
+            self.accepted[vertex] = True
+
+    def set_start(self, _start: int) -> None:
+        assert 0 <= _start < self.n
+        self.start = _start
+
+    def execute(self, array):
+        assert self.start != -1
+        current: int = self.start
+        for val in array:
+            if self.transitions[current][val] == -1:
+                return False
+            current = self.transitions[current][val]
+        return self.accepted[current]
+
